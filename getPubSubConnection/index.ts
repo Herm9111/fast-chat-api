5fb9703a4a8c8188470ec7511c6cd712ab74f944
Mau Herrera <herm9111@gmail.com>
2022-04-04 22:33:36 -0700

    Changed token generation method and added roles

--- getPubSubConnection/index.ts
+++ getPubSubConnection/index.ts
@@ -1,13 +1,22 @@
 import { AzureFunction, Context, HttpRequest } from "@azure/functions"
+const { WebPubSubServiceClient } = require("@azure/web-pubsub");
 
 const httpTrigger: AzureFunction = async function (context: Context, req: HttpRequest, connection): Promise<void> {
+    const serviceClient = new WebPubSubServiceClient(process.env['WebPubSubConnectionString'], "fastChat");
+    const token = await serviceClient.getClientAccessToken({
+         userId: req.headers['headers.x-ms-client-principal-name'],
+         roles: [ "webpubsub.sendToGroup", "webpubsub.joinLeaveGroup" ]
+        });
+
     context.res = {
         status: 200, /* Defaults to 200 */
         headers: {
             'Content-Type': 'application/json'
         },
         body: {
-            connection
+            accessToken: token,
+            baseUrl: "wss://fast-chat-ps.webpubsub.azure.com/client/hubs/fastChat",
+            url: `wss://fast-chat-ps.webpubsub.azure.com:443/client/hubs/fastChat?access_token=${token}`
         }
     };
 
