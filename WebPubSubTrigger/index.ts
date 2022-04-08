076dde17a59dc7a78e44112884d198b629306fc7
Mau Herrera <herm9111@gmail.com>
2022-04-07 21:06:14 -0700

    Added response

--- WebPubSubTrigger/index.ts
+++ WebPubSubTrigger/index.ts
@@ -3,6 +3,9 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 const WebPubSubTrigger: AzureFunction = async function (context: Context, req: HttpRequest, data, pubSubOut): Promise<void> {
     context.log('HTTP trigger function processed a request.');
 
+    context.res = {
+        status: 200, /* Defaults to 200 */
+    };
 };
 
 export default WebPubSubTrigger;
