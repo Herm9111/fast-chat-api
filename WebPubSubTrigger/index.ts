bfa673155e04abe0b4e8630ad5927ee6d16a0730
Mau Herrera <herm9111@gmail.com>
2022-04-07 21:01:47 -0700

    Emptied function

--- WebPubSubTrigger/index.ts
+++ WebPubSubTrigger/index.ts
@@ -2,10 +2,6 @@ import { AzureFunction, Context, HttpRequest } from "@azure/functions"
 
 const WebPubSubTrigger: AzureFunction = async function (context: Context, req: HttpRequest, data, pubSubOut): Promise<void> {
     context.log('HTTP trigger function processed a request.');
-    
-    console.log('Request from: ', context.bindingData.request.connectionContext.userId);
-    console.log('Request message data: ', data);
-    console.log('Request message dataType: ', context.bindingData.request.dataType);
 
 };
 
